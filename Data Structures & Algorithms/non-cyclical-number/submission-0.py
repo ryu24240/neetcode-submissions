452d3b430c1354585d00b42e8950d40296c22934
Ryuho Kanno <149505871+ryu24240@users.noreply.github.com>
2026-09-10 12:18:46 +0900

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            sum = 0
+            while n > 0:
+                digit = n % 10
+                sum += digit * digit
+                n //= 10
+            
+            n = sum
+
+        return n == 1
+        
+
