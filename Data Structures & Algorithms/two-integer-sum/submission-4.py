77350c428f472ef465f32d6fca82d6abae091a85
Ryuho Kanno <149505871+ryu24240@users.noreply.github.com>
2026-09-13 14:18:15 +0900

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        counts = {}
+
+        if len(nums) == 2:
+            return [0, 1]
+
+        for i, num in enumerate(nums):
+            remainder = target - num
+            if remainder in counts.keys():
+                return [min(i, counts[remainder]), max(i, counts[remainder])]
+
+            if num not in counts:
+                counts[num] = nums.index(num)
